5c5e44646b475bca99aefe35289da4265ac5eec4
comar <saywooong@gmail.com>
2022-11-20 14:37:17 +0900

    update: trivial

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from skimage.segmentation import slic
-from skimage.segmentation import mark_boundaries
 from skimage.util import img_as_float
 from skimage import io
 import skimage.measure
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
+
+def parse():
+  p = argparse.ArgumentParser()
+  p.add_argument("--winsize", type=int, default=16)
+  p.add_argument("--path_input", type=str, default="sample01_resize_256_256.jpg")
+  p.add_argument("--sigma", type=float, default=5.0)
+  p.add_argument("--num_seg", type=int, default=100)
+  return p.parse_args()
+
 
 
 def dominant_pool_2d(spatial: np.ndarray, winsize=16):
@@ -17,7 +26,7 @@ def dominant_pool_2d(spatial: np.ndarray, winsize=16):
 
   Parameters
   ----------
-  spatial : int ndarray of shape (Width, Hight)
+  spatial : int ndarray of shape (width, hight)
     The spatial is represented by int label, not one-hot encoding
   winsize : int, optional
     Length of sweeping window
@@ -25,17 +34,7 @@ def dominant_pool_2d(spatial: np.ndarray, winsize=16):
   Returns
   -------
   pool : ndarray of shape (N,M)
-    The pooling results
-
-  Examples
-  --------
-  >>> np.eye(2, dtype=int)
-  array([[1, 0],
-         [0, 1]])
-  >>> np.eye(3, k=1)
-  array([[0.,  1.,  0.],
-         [0.,  0.,  1.],
-         [0.,  0.,  0.]])
+    The pooling results.
 
   """
   num_seg = spatial.max() + 1
@@ -49,6 +48,19 @@ def dominant_pool_2d(spatial: np.ndarray, winsize=16):
 def gen_sp2rgbmean(image: np.ndarray, segments: np.ndarray):
   """
   Generate a mapping from superpixel index to the corresponding RGB mean value
+
+  Parameters
+  ----------
+  image : image ndarray of shape (width, hight, dim_spectrum)
+    The spatial is represented by int label, not one-hot encoding
+  segments : int ndarray of shape (width, hight)
+    Segmentation labels for each pixel
+
+  Returns
+  -------
+  sp2rgb : ndarray of shape (N, dim_spectrum)
+    Mapping of superpixel index to RGB mean value.
+    N is the number of segmentation label.
   """
 
   # Define required values
@@ -71,70 +83,15 @@ def gen_sp2rgbmean(image: np.ndarray, segments: np.ndarray):
   return sp2rgb
 
 
-def gen_mean_superpixel(image, num_seg=100, sigma=5):
+def main1():
   """
-  SLIC
+  Estimate superpixels with mean color.
   """
-  image_flat = image.reshape(-1, 3)
-  w, h, _ = image.shape
-  dim_spatial = w * h
-
-  segments = slic(image, n_segments=num_seg, sigma=sigma)
-
-  # Redefine the number of superpixel after the SLIC estimation
-  num_seg = segments.max() + 1
-
-  # Flatten spatial dimension to improve visibility
-  segments_flat = segments.reshape(-1)
-
-  # Convert label to one-hot encoding
-  one_hot = np.eye(num_seg)[segments_flat]
-  one_hot_sum = one_hot.sum(0)
-
-  # Calculate mean RGB value according to the label
-  pix_lab_rgb = one_hot.reshape(dim_spatial, num_seg, 1) * image_flat.reshape(
-      dim_spatial, 1, 3)
-  sp2rgb = pix_lab_rgb.sum(0)
-  one_hot_sum_reshape = one_hot_sum.reshape(num_seg, 1).repeat(3, 1)
-  sp2rgb = sp2rgb / one_hot_sum_reshape
-
-  # Replace all RGB value with mean RGB value
-  image_meansp = one_hot[..., None] * sp2rgb
-  image_meansp = image_meansp.sum(-2)
-  image_meansp = image_meansp.reshape(w, h, 3)
-
-  return image_meansp, segments, sp2rgb
-
-
-def main():
-  image = img_as_float(io.imread('sample01_resize_256_256.jpg'))
-
-  num_seg = 100
-  image_meansp, segments, sp2rgb = gen_mean_superpixel(image, num_seg=num_seg)
-  grid = dominant_pool_2d(segments, num_seg=num_seg)
-  dominant_color = sp2rgb[grid]
-
-  fig = plt.figure("Superpixels -- %d segments" % (num_seg))
-  ax = fig.add_subplot(1, 2, 1)
-  ax.set_title("Superpixel with mean color")
-  ax.imshow(mark_boundaries(image, segments))
-  ax.imshow(image_meansp)
-  plt.axis("off")
-
-  ax = fig.add_subplot(1, 2, 2)
-  ax.set_title("dominant_color")
-  ax.imshow(dominant_color)
-  plt.axis("off")
-
-  plt.tight_layout()
-  plt.show()
-
-
-def main2():
-  image = img_as_float(io.imread('sample01_resize_256_256.jpg'))
-  num_seg = 100
-  sigma = 5
-  winsize = 16
+  args = parse()
+  image = img_as_float(io.imread(args.path_input))
+  num_seg = args.num_seg
+  sigma = args.sigma
+  winsize = args.winsize
 
   # What we exactly needs ====================================================
   segments = slic(image, n_segments=num_seg, sigma=sigma)
@@ -154,5 +111,32 @@ def main2():
   plt.show()
 
 
+def main2():
+  """
+  Estimate dominant colors for each spatial region.
+  """
+  args = parse()
+  image = img_as_float(io.imread(args.path_output))
+  num_seg = args.num_seg
+  sigma = args.sigma
+
+  # What we exactly needs ====================================================
+  segments = slic(image, n_segments=num_seg, sigma=sigma)
+  sp2rgbmean = gen_sp2rgbmean(image, segments)  # The most important data
+  superpixel = sp2rgbmean[segments]
+  # print(dominant_color.shape)  # (w/16, h/16, 3)
+  # ==========================================================================
+
+  fig = plt.figure("hello")
+  ax = fig.add_subplot(1, 1, 1)
+  ax.set_title("superpixel")
+  ax.imshow(superpixel)
+  plt.axis("off")
+
+  plt.tight_layout()
+  plt.show()
+
+
 if __name__ == "__main__":
+  main1()
   main2()
